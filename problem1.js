ed9d18fa5226543bfdce2c417409b14cc06c68f5
Nabila Nuzhat Moin <e.nabilanuzhat@gmail.com>
2024-03-13 01:19:57 +0600

    for loop in sorry function

--- problem1.js
+++ problem1.js
@@ -1,5 +1,11 @@
-// print sorry 10 times
+// write a function to print sorry 10 times
 
-for(var i= 0; i < 10; i++ ){
-    console.log("sorry!!!");
+
+
+function writeSorry(){
+    for(var i= 0; i < 10; i++ ){
+        console.log("sorry!!!");
+    }
 }
+
+writeSorry();
